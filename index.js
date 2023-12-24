36d1593c65f830b3d0f70d39d56dff4e671fc4ff
Vladimir Kirpichev <42208172+djbobah@users.noreply.github.com>
2023-12-24 11:56:11 +0300

    arrays done

--- index.js
+++ index.js
@@ -1,121 +1,103 @@
+function getRandom(max, min = 0) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 // task1
 const task1 = document.querySelector(".task1");
 
-const pet = {
-  name: "Бобик",
-  type: "пес",
-  sound: "гав",
-  say: function (phrase) {
-    return `${this.type} ${this.name} говорит ${
-      phrase === "" ? this.sound : phrase
-    }`;
-  },
-};
+let nums = [5, 2, 4, 6, 7];
+let squares = [];
+nums[99] = 3;
 
-// console.log(pet.name);
-// console.log(pet.say("мяу"));
-task1.innerHTML += `Задача 1: pet.say("мяу") = ${pet.say("мяу")}`;
-console.log(`Задача 1: pet.say("мяу") = ${pet.say("мяу")}`);
+nums.map((num) => {
+  if (num !== undefined) {
+    squares.push((num *= num));
+  }
+});
+
+// console.log(nums);
+// console.log(squares);
+
+task1.innerHTML += ` массив nums = [5, 2, 4, 6, 7]. Ответ [${squares}]`;
+console.log(`Задача 1:  массив nums = [5, 2, 4, 6, 7]. Ответ [${squares}]`);
 
 // task2
 const task2 = document.querySelector(".task2");
-const fruits = {
-  potato: "овощи",
-  carrot: "овощи",
-  banana: "фрукты",
-  orange: "фрукты",
-  apple: "фрукты",
-  strawberry: "ягоды",
-  tomato: "овощи",
-  melon: "фрукты",
-  lemon: "фрукты",
-};
-let sumVegetables = {};
-for (let fruit in fruits) {
-  if (sumVegetables[fruits[fruit]]) {
-    sumVegetables[fruits[fruit]]++;
-  } else sumVegetables[fruits[fruit]] = 1;
+const list = [
+  { product: "Apple", price: 25 },
+  { product: "Cherry", price: 32 },
+  { product: "Strawberry", price: 45 },
+  { product: "Pineapple", price: 45 },
+];
+
+let maxPrice = 0,
+  maxIdx = 0;
+for (let i = 0; i < list.length; i++) {
+  if (list[i].price > maxPrice) {
+    maxPrice = list[i].price;
+    maxIdx = i;
+  }
 }
-task2.innerHTML += `Задача 2: ${JSON.stringify(sumVegetables)}`;
-console.log("Задача 2: ", sumVegetables);
+console.log(list[maxIdx]);
+
+task2.innerHTML += ` "${list[maxIdx].product}"`;
+console.log(
+  `Задача 2: ${JSON.stringify(list)}.  Ответ: "${list[maxIdx].product}"`
+);
 
 // task3
 
+// const task3_1 = document.querySelector(".task3_1");
+const products = [
+  "Апельсины",
+  "Мандарины",
+  "Яблоки",
+  "Груши",
+  "Картошка",
+  "Помидоры",
+  "Огурцы",
+  "Лук",
+  "Сметана",
+  "Майонез",
+];
+
+let basket = [];
+
+for (let i = 0; i <= getRandom(30, 10); i++) {
+  const currentProduct = getRandom(products.length);
+  let present = false;
+
+  for (let j = 0; j < basket.length; j++) {
+    if (basket[j].product === products[currentProduct]) {
+      basket[j].cnt += 1;
+      present = true;
+    }
+  }
+  if (!present) {
+    basket.push({
+      product: products[currentProduct],
+      price: getRandom(200, 50),
+      cnt: 1,
+    });
+  }
+}
+
+const sum = basket.reduce((acc, item) => {
+  acc += item.price * item.cnt;
+  return acc;
+}, 0);
+
+console.log("Задача 3:", "Корзина", basket, "Сумма покупок:", sum);
+
 const task3 = document.querySelector(".task3");
-const task3_1 = document.querySelector(".task3_1");
-const cars = {
-  lada: {
-    color: "red",
-    country: "Russia",
-    year: 2000,
-  },
-  opel: {
-    color: "black",
-    country: "Germany",
-    year: 2010,
-  },
-  toyota: {
-    color: "silver",
-    country: "Japan",
-    year: 2011,
-  },
-  honda: {
-    color: "white",
-    country: "Japan",
-    year: 2020,
-  },
-  nissan: {
-    color: "grey",
-    country: "Japan",
-    year: 2019,
-  },
-  volkswagen: {
-    color: "blue",
-    country: "German",
-    year: 2005,
-  },
-  BMV: {
-    color: "red",
-    country: "German",
-    year: 2006,
-  },
-  audi: {
-    color: "blue",
-    country: "German",
-    year: 2007,
-  },
-  citroen: {
-    color: "lightblue",
-    country: "France",
-    year: 2009,
-  },
-  renault: {
-    color: "yellow",
-    country: "France",
-    year: 2013,
-  },
-};
-let sortedCars = [];
+task3.style.borderRadius = "10px";
+task3.style.padding = "10px";
+task3.style.fontSize = "14px";
+task3.style.lineHeight = "24px";
+task3.style.backgroundColor = "black";
+task3.style.color = "white";
+task3.style.fontFamily = "Sans";
 
-for (let car in cars) {
-  sortedCars.push([car, cars[car]]);
-}
-sortedCars.sort(function (a, b) {
-  return a[1].year - b[1].year;
-});
-
-console.log("Задача 3:");
-console.log("Самые старые автомобили:");
-for (let i = 0; i < 3; i++) {
-  console.log(sortedCars[i][0], sortedCars[i][1].year, "года выпуска");
-  task3.innerHTML += `${sortedCars[i][0]} ${sortedCars[i][1].year} года выпуска;
-  `;
-}
-
-console.log("Самые молодые автомобили:");
-for (let i = sortedCars.length - 3; i < sortedCars.length; i++) {
-  console.log(sortedCars[i][0], sortedCars[i][1].year, "года выпуска");
-  task3_1.innerHTML += `${sortedCars[i][0]} ${sortedCars[i][1].year} года выпуска;
-  `;
-}
-// console.log(sortedCars);
+task3.innerHTML += `${JSON.stringify(
+  basket
+)} <br><br> Сумма покупок: ${sum} р.`;
